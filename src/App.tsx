8ba6dff4289313767898d9a76289ea77cafb595d
ZubkovAC <3y6kob90@gmail.com>
2021-07-18 23:52:43 +0300

    calendar (layout)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Calendar} from "./calendar/calendar";
+import {ListEmployee} from "./listEmployee/listEmployee";
 
 function App() {
     const [edit, setEdit] = useState(true)
     return (
         <div className="App">
-
-            {
-                edit && <Calendar/>
-            }
+            <ListEmployee />
+            {/*{*/}
+            {/*    edit && <Calendar/>*/}
+            {/*}*/}
 
         </div>
     );
--- src/listEmployee/listEmployee.tsx
+++ src/listEmployee/listEmployee.tsx
@@ -1,9 +1,114 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Input, Table, Space, Tag, Button} from "antd";
+import 'antd/dist/antd.css';
+import  css from './listEmployee.module.css'
+import {Calendar} from "../calendar/calendar";
+
+
+type ColumnsType = {
+    key: string
+    name: string
+    age: number
+    address: string
+    tags: Array<string>
+}
+
+
+
+
+
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: '0',
+        tags: ['fronted','junior'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: '0',
+        tags: ['backend','middle'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: '0',
+        tags: ['director'],
+    },
+];
 
 export const ListEmployee = () => {
-    return (
-        <div>
+    const [edit, setEdit] = useState(false)
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'Name',
+            render: (text: ColumnsType) => <a >{text}</a>,
+        },
+        {
+            title: 'Current Position',
+            key: 'Current Position',
+            dataIndex: 'tags',
+            render: (tags: any) => (
+                <>
+                    {tags.map((tag: string) => {
+                        let color = tag === 'fronted' || tag === 'backend'   ?  'green' : 'geekblue' ;
+                        if (tag === 'junior') {
+                            color = 'volcano';
+                        }
+                        if (tag === 'director') {
+                            color = 'grey';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Salary / hour',
+            dataIndex: 'age',
+            key: 'Salary / hour',
+        },
+        {
+            title: 'bonus (%)',
+            key: 'bonus',
+            render: (text: ColumnsType, record: any) => (
+                <Space size="middle">
+                    <Input placeholder={'0'} type={"number"} style={{ width: 100 }}/>
+                </Space>
+            ),
+        },
+        {
+            title: 'monthly salary',
+            dataIndex: 'address',
+            key: 'monthly salary',
+            render: (text: ColumnsType, record: any) => (
+                <div>
+                    <span style={{marginRight:'20px'}}>{text}</span>
+                    <Button type="primary" onClick={()=>setEdit(!edit)}>Primary Button</Button>
+                </div>
+            )
 
+        },
+    ];
+    return (
+        <div className={css.containerList}>
+            <div style={{display:'flex',marginLeft:'15px'}}>
+                <Input placeholder="Name" style={{ width: 400 }} />
+            </div>
+            <div>
+                <Table columns={columns} dataSource={data}/>
+                {edit && <Calendar/>}
+            </div>
         </div>
     );
 };
